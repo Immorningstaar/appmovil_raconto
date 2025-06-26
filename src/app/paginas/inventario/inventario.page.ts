997346286fcbe9d4323fe10c625e8865c04a1ebf
psyconach <ignacio.jana95@gmail.com>
2025-06-25 22:57:11 -0400

    pag inventario mockup terminada

--- src/app/paginas/inventario/inventario.page.ts
+++ src/app/paginas/inventario/inventario.page.ts
@@ -1,18 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-inventario',
   templateUrl: './inventario.page.html',
   styleUrls: ['./inventario.page.scss'],
   standalone: false,
 })
-export class InventarioPage implements OnInit {
-  
-  
+export class InventarioPage {
+editar(producto: any) {
+  console.log('Editar producto:', producto);
+  // ARREGLAR MAS ADELANTE EN LA BASE DE DATOS
+}
 
-  constructor() { }
 
-  ngOnInit() {
-  }
-
+  productos = [
+    {
+      nombre: 'Queso mantecoso',
+      categoria: 'Lácteos',
+      stock: 12,
+      unidad: 'kg',
+      vencimiento: '2025-07-01',
+    },
+    {
+      nombre: 'Pechuga de pollo',
+      categoria: 'Carnes',
+      stock: 8,
+      unidad: 'kg',
+      vencimiento: '2025-06-25',
+    },
+    {
+      nombre: 'Jugo natural',
+      categoria: 'Bebidas',
+      stock: 20,
+      unidad: 'botellas',
+      vencimiento: '2025-07-10',
+    },
+  ];
 }
